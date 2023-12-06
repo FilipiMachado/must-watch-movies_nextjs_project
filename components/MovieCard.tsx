c19efbb7ac7b60f8fa0b3eeb18b126bb8b2181e7
filipi <filipisee@hotmail.com>
2023-12-05 21:23:19 -0300

    added react intersection observer package

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,22 +1,5 @@
 import Image from "next/image";
 
-/* 
-adult: false,
-backdrop_path: '/vRx6kzCItpqwnYCcj2z8qf70gv3.jpg',
-genre_ids: [Array],
-id: 10908,
-original_language: 'en',
-original_title: 'Inferno',
-overview: 'Eddie Lomax (Jean-Claude Van Damme) is a drifter who has been in a suicidal funk since the death of his close friend Johnny (Danny Trejo). Riding his motorcycle into a small desert town where Johnny once lived, Lomax is confronted by a gang of toughs, who beat him and steal his bike. However, Lomax is not a man to take an injustice lying down, and soon he begins exacting a violent revenge on the men who stole his motorcycle, with local handyman Jubal Early (Pat Morita) lending a hand and several area ladies offering aid and comfort.',
-popularity: 615.686,
-poster_path: '/dFlI0Vb4JOsRXG1JSS2Ufs6Sp8k.jpg',
-release_date: '1999-09-25',
-title: 'Inferno',
-video: false,
-vote_average: 6.104,
-vote_count: 275
-*/
-
 export interface MovieProp {
   id: string;
   title: string;
@@ -31,7 +14,7 @@ interface Prop {
   index: number;
 }
 
-function AnimeCard({ movie }: Prop) {
+function MovieCard({ movie }: Prop) {
   function formatVoteAverage(voteAverage: number) {
     return parseFloat(voteAverage.toFixed(1));
   }
@@ -43,7 +26,7 @@ function AnimeCard({ movie }: Prop) {
           src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
           alt={movie.title}
           fill
-          className="rounded-xl"
+          className="rounded-xl cursor-pointer"
         />
       </div>
       <div className="py-4 flex flex-col gap-3">
@@ -78,7 +61,9 @@ function AnimeCard({ movie }: Prop) {
               height={18}
               className="object-contain"
             />
-            <p className="text-base font-bold text-[#FFAD49]">{formatVoteAverage(movie.vote_average)}</p>
+            <p className="text-base font-bold text-[#FFAD49]">
+              {formatVoteAverage(movie.vote_average)}
+            </p>
           </div>
         </div>
       </div>
@@ -86,4 +71,4 @@ function AnimeCard({ movie }: Prop) {
   );
 }
 
-export default AnimeCard;
+export default MovieCard;
