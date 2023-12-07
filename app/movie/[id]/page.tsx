2468fc551fd1599ad111ff8835b413a5b5952324
filipi <filipisee@hotmail.com>
2023-12-07 16:11:10 -0300

    finished get movie details feature

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -1,11 +1,36 @@
+"use client";
+
+import axios from "axios";
+import { useParams } from "next/navigation";
+
 export default function MovieIdPage() {
+  const params = useParams();
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMzQ2MDlmZDFhNzgyMzcyZjE1MGM0MGFkODQ2MTZkZiIsInN1YiI6IjYwZTRiOGE3ODNlZTY3MDA3NGMxM2JiZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SG4-z0rE2UK7HpvbFJ68F1Ym5u1nRdX9Lx8YvX2WWgo'
+    }
+  };
+
+  fetch(
+    `https://api.themoviedb.org/3/movie/${params.id}?language=en-US`,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response)
+    })
+    .catch((err) => console.error(err));
+
   return (
     <div className="sm:p-16 py-16 px-8 gap-10">
       <div className="flex justify-between h-[80vh]">
-        <div className="w-[40vw] bg-slate-500/50 rounded-l">
+        <div className="w-[40vw] bg-slate-500/50 rounded-l-lg">
           <p>BACKGROUND IMAGE</p>
         </div>
-        <div className="w-[60vw] py-28 p-6 bg-gray-400 rounded-r">
+        <div className="w-[60vw] py-28 p-6 bg-gray-400 rounded-r-lg">
           <div className="mb-20">
             <p className="mb-8 text-4xl">Movie Title</p>
             <p className="font-bold text-md mb-2">SYNOPSIS</p>
--- app/page.tsx
+++ app/page.tsx
@@ -10,16 +10,17 @@ async function Home() {
   return (
     <>
       <Hero />
+      
       <main className="sm:p-16 py-16 px-8 flex flex-col gap-10">
         <h2 className="text-3xl text-white font-bold">
           Discover <span className="text-red-600">Movies</span>
         </h2>
-
         <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
           {data}
         </section>
         <LoadMore />
       </main>
+
       <Footer />
     </>
   );
